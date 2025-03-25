a65362e41101d0106b641e6cb5f95598ae00594a
ronard1 <universia0503@gmail.com>
2025-03-25 05:06:41 -0500

    3. Entidades y acceso a datos

---
+++ src/main/java/pe/com/ron/persistence/entity/CardEntity.java
@@ -0,0 +1,14 @@
+package pe.com.ron.persistence.entity;
+
+
+import lombok.Data;
+
+@Data
+public class CardEntity {
+
+    private Long id;
+    private String title;
+    private String description;
+    private BoardColumnEntity boardColumn = new BoardColumnEntity();
+
+}
---
+++ src/main/java/pe/com/ron/service/BoardService.java
@@ -0,0 +1,30 @@
+package pe.com.ron.service;
+
+import lombok.AllArgsConstructor;
+import pe.com.ron.persistence.dao.BoardDAO;
+import pe.com.ron.persistence.entity.BoardEntity;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+@AllArgsConstructor
+public class BoardService {
+
+    private final Connection connection;
+
+    public boolean delete(final Long id) throws SQLException {
+        var dao = new BoardDAO(connection);
+        try{
+            if (!dao.exists(id)) {
+                return false;
+            }
+            dao.delete(id);
+            connection.commit();
+            return true;
+        } catch (SQLException e) {
+            connection.rollback();
+            throw e;
+        }
+    }
+
+}
